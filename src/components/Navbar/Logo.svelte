2e14f99a28b7d9e5c1c510b14385446189abce95
MeirJC <meir84@gmail.com>
2024-09-12 00:27:09 +0300

    Links fix

--- src/components/Navbar/Logo.svelte
+++ src/components/Navbar/Logo.svelte
@@ -3,7 +3,12 @@
   export let nanostoresLogo: string;
 </script>
 
-<a class="flex items-center gap-2 rounded-xl bg-gray-500/40 p-1" href="/" title="Home">
+<a
+  class="flex items-center gap-2 rounded-xl bg-gray-500/40 p-1"
+  href="/"
+  title="Home"
+  aria-label="Navigate back to the home page"
+>
   <img src={astroLogo} alt="Astro logo" class="w-9 h-9" />
   <span class="block self-center bg-amber-400 bg-clip-text pb-2 text-4xl font-bold text-transparent"
     >+</span
